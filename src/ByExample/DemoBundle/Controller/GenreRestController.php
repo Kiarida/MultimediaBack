027c0dd9cd1771092ec4320e40bf1be667a29ab0
Myriam <ethilanie@gmail.com>
2014-12-10 15:15:55 +0100

    Ajout controller genres

---
+++ src/ByExample/DemoBundle/Controller/GenreRestController.php
@@ -0,0 +1,9 @@
+<?php namespace ByExample\DemoBundle\Controller;
+
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+use FOS\RestBundle\Controller\Annotations\NamePrefix;
+use FOS\RestBundle\View\View AS FOSView;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use JMS\SecurityExtraBundle\Annotation\Secure;
+?>
